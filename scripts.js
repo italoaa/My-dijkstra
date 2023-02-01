a53513942cf01d8847dd5c2e2f6f31abf2f72d78
Italo Amaya <italo@Italos-MacBook-Pro.local>
2023-02-01 18:51:08 +0000

    Achived the exporting of the grid to array representation

--- scripts.js
+++ scripts.js
@@ -1,13 +1,49 @@
 // function that builds a grid in the "container"
 function createGrid(x) {
+    var x1 = Math.floor(Math.random() * x);
+    var y1 = Math.floor(Math.random() * x);
+    var x2 = Math.floor(Math.random() * x);
+    var y2 = Math.floor(Math.random() * x);
     for (var rows = 0; rows < x; rows++) {
         for (var columns = 0; columns < x; columns++) {
-            $("#container").append("<div class='grid'></div>");
+            if (rows == x1 && columns == y1) {
+                $("#container").append("<div id='start' class='grid' style='background-color:red'></div>");
+            } else if (rows == x2 && columns == y2){
+                $("#container").append("<div id='end' class='grid' style='background-color:blue'></div>");
+            } else {
+                $("#container").append("<div id='normal' class='grid'></div>");
+            }
         };
     };
     $(".grid").width(960/x);
     $(".grid").height(960/x);
+    gridToArray(x)
 };
+
+function gridToArray(n){
+    // this function takes the grid and makes it an array
+    var counter = -1;
+    var grid = [];
+    $("#container").find('div').each(function () {
+        // Counter tells the id of the node
+        counter += 1;
+
+        // Init connections
+        var connections = [];
+
+        var totalNodes = n*n;
+
+        for (var node = 0; node < (totalNodes - 1); node++) {
+            if ((node == (counter-1)) || (node == (counter+1)) || (node == (counter+n)) || (node == (counter-n))){
+                connections.push(1);
+            } else {
+                connections.push(0);
+            }
+        }
+        grid.push(connections);
+    })
+    console.log(grid);
+}
 
 // function that clears the grid
 function clearGrid(){
@@ -26,33 +62,18 @@ function refreshGrid(){
 // creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
 // allows the click of a button to prompt the user to create a new grid
 $(document).ready(function() {
-    createGrid(16);
+    createGrid(5);
 
-    $(".grid").mouseover(function() {
-        $(this).css("background-color", "grey");
-    });
 
-    $(".grid").mouseout(function() {
-        $(this).css("background-color", "white");
-    });
     $(".grid").click(function() {
-        console.log($(this).css('background-color'))
-        if ($(this).css('background-color') == "rgba(128, 128, 128)") {
-            $(this).css("background-color", "black");
-        } else if ($(this).css('background-color') == "rbg(0, 0, 0)") {
-            $(this).css("background-color", "grey");
-        }
+        $(this).css("background-color", "black");
     });
 
     $(".newGrid").click(function() {
         refreshGrid();
 
-        $(".grid").mouseover(function() {
-            $(this).css("background-color", "grey");
+        $(".grid").mousedown(function() {
+            $(this).css("background-color", "black");
         });
-
-        $(".grid").mouseout(function() {
-            $(this).css("background-color", "white");
-            });
     });
 });
